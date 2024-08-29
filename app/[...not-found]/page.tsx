f6e3e4692608dc8cb183b9f4ec0b77678e6956ec
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-08-29 23:28:38 +0300

    chore: Add 404 page for not found routes

---
+++ app/[...not-found]/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "404",
+  description: "Something went wrong",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
+      <h1 className="text-2xl-semi text-ui-fg-base text-black">
+        Page not found
+      </h1>
+      <p className="text-small-regular text-ui-fg-base  text-black">
+        The page you tried to access does not exist.
+      </p>
+      <Link className="flex gap-x-1 items-center group  text-black" href="/">
+        <span className="text-ui-fg-interactive ">Go to frontpage</span>
+
+        <svg
+          className="group-hover:rotate-45 ease-in-out duration-150 " 
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+}
