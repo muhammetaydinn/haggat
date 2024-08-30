85c04ece29e54e22e32336eefc8358c6f458e3e1
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-08-30 04:52:30 +0300

    chore: Update tailwind.config.ts and package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,8 @@
 "use client"; // Bunu ekleyerek bileşeni Client Component yapıyoruz
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isWide, setIsWide] = useState(false);
-    const router = useRouter();
-
-  useEffect(() => {
-    // Ekran genişliğini kontrol eden bir fonksiyon
-    const handleResize = () => {
-      setIsWide(window.innerWidth > 500);
-    };
-
-    // Sayfa yüklendiğinde ve ekran boyutu değiştiğinde kontrol et
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup: event listener'ı kaldır
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="bg-gray-50 min-h-screen">
-      {/* Üst Navigasyon Barı */}
-      <header className="flex justify-between items-center p-5 border-b border-gray-300 bg-white">
-        
-        
-        <div className="flex items-center">
-          <Image
-            src={
-              // if screen is small then logo.svg else logo-small.svg
-              isWide ? "/logo.svg" : "/logosmall.svg"
-            }
-            alt="Logo"
-            width={isWide ? 100 : 32}
-            height={isWide ? 32 : 32}
-            style={{ minWidth: isWide ? "80px" : "32px" }}
-            className="mr-3"
-          />
-        </div>
-        <div className="flex items-center">
-          {/* <a href="#" className="ml-5 text-gray-900 hover:text-indigo-600">
-            Link 1
-          </a>
-          <a href="#" className="ml-5 text-gray-900 hover:text-indigo-600">
-            Link 2
-          </a>
-          <a href="#" className="ml-5 text-gray-900 hover:text-indigo-600">
-            Link 3
-          </a> */}
-          <input
-            type="text"
-            placeholder="Ara..."
-            className="ml-5 p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-600"
-          />
-          <button
-            onClick={
-              () => {
-               router.push("/login")
-              }
-            }
-            className="ml-5 p-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
-            Giriş Yap
-          </button>
-        </div>
-      </header>
-
       {/* Ana İçerik */}
       <main className="p-5">
         <div className="p-10 bg-indigo-600 text-white text-center text-xl mb-5 rounded">
