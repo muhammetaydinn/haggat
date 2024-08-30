0a9d13f8d958b472da4ec111815df1e96282f77a
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-08-31 02:42:18 +0300

    chore: Update HomeCarousel component and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,21 @@
 "use client";
 import HomeCarousel from "@/components/HomeCarousel";
-import { fetchAllCategories, fetchHomeCarouselProducts, fetchProductsByCategoryId } from "@/services";
+import {
+  fetchAllCategories,
+  fetchHomeCarouselProducts,
+  fetchProductsByCategoryId,
+} from "@/services";
 
- // Bunu ekleyerek bileşeni Client Component yapıyoruz
+// Bunu ekleyerek bileşeni Client Component yapıyoruz
 
 export default async function Home() {
-    const initialData = await fetchHomeCarouselProducts();
-    const initialCategoriesData = await fetchAllCategories();
+  const initialData = await fetchHomeCarouselProducts();
+  const initialCategoriesData = await fetchAllCategories();
   return (
     <div className="bg-gray-50 min-h-screen">
       {/* Ana İçerik */}
       <main className="p-5">
-        <div className="p-10 bg-indigo-600 text-white text-center text-xl mb-5 rounded">
-          Banner burada yer alacak
-        </div>
         <HomeCarousel initialData={initialData} />
-        <div className="grid grid-cols-3 gap-5">
-          <div className="p-5 bg-white border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow">
-            Kategori 1
-          </div>
-          <div className="p-5 bg-white border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow">
-            Kategori 2
-          </div>
-          <div className="p-5 bg-white border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow">
-            Kategori 3
-          </div>
-        </div>
       </main>
     </div>
   );
--- components/HomeCarousel.tsx
+++ components/HomeCarousel.tsx
@@ -1,18 +1,65 @@
 import { ICategory } from '@/models/category.model';
 import { IProduct } from '@/models/product.model';
 import React from 'react'
+import { Pagination,Navigation } from "swiper/modules";
+
+// core version + navigation, pagination modules:
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 interface ICardsCarouselProps {
   initialData: IProduct[];
 }
 const HomeCarousel = ({ initialData }: ICardsCarouselProps) => {
-    return (initialData?.length > 0) ?
-        initialData.map((product) => (
-            <div key={product.id} className="p-5 bg-white border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow text-black">
-                <img src={product.images[0]} alt={product.title} className="w-20 h-20 object-cover" />
-                <p>{product.category.name}</p>
+    return initialData?.length > 0 ? (
+      <div className="p-1 bg-gray-800">
+        <Swiper
+          loop={true}
+          // install Swiper modules
+          modules={[Pagination, Navigation]}
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true }}
+        >
+          {initialData.map((product) => (
+              <SwiperSlide key={product.id}
+                  
+                  style={{
+                      accentColor: "red",
+                      
+                  }}
+                  
+              >
+              <div className="p-5 bg-gray-100 border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow text-black">
+                <img
+                  src={product.images[0]}
+                  alt={product.title}
+                  className="w-20 h-20 object-cover"
+                />
+                <p>{product.title}</p>
                 <p>{product.price}</p>
-            </div>
-        )):(<div className='text-black'>Yükleniyor...</div>)
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    ) : (
+      // initialData.map((product) => (
+      //     <div key={product.id} className="p-5 bg-white border border-gray-300 rounded text-center shadow hover:shadow-lg transition-shadow text-black">
+      //         <img src={product.images[0]} alt={product.title} className="w-20 h-20 object-cover" />
+      //         <p>{product.category.name}</p>
+      //         <p>{product.price}</p>
+      //     </div>
+      // ))
+      <div className="text-black">Yükleniyor...</div>
+    );
       
  
 };
