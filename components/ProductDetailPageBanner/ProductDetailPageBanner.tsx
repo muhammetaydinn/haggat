1021a3b5c613534448b5a14e5df88d0f7a7d25e7
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-09-04 05:04:28 +0300

    feat:added fake rating to product detail

--- components/ProductDetailPageBanner/ProductDetailPageBanner.tsx
+++ components/ProductDetailPageBanner/ProductDetailPageBanner.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   Image,
   Card,
@@ -10,19 +10,22 @@ import {
   SimpleGrid,
   useMantineTheme,
   ActionIcon,
-} from '@mantine/core';
-import { Carousel } from '@mantine/carousel';
-import { IconShoppingCart } from '@tabler/icons-react';
-import classes from './ProductDetailPageBanner.module.css';
-import { IProduct } from '@/models/product.model';
-import { useCartStore } from '@/store/cart';
-import { useState } from 'react';
+  Rating,
+} from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import { IconShoppingCart } from "@tabler/icons-react";
+import classes from "./ProductDetailPageBanner.module.css";
+import { IProduct } from "@/models/product.model";
+import { useCartStore } from "@/store/cart";
+import { useState } from "react";
 
 interface ProductDetailPageBannerProps {
   productDetailData: IProduct;
 }
 
-export function ProductDetailPageBanner({ productDetailData }: ProductDetailPageBannerProps) {
+export function ProductDetailPageBanner({
+  productDetailData,
+}: ProductDetailPageBannerProps) {
   const theme = useMantineTheme();
 
   const slides = productDetailData?.images?.map((image) => (
@@ -36,8 +39,12 @@ export function ProductDetailPageBanner({ productDetailData }: ProductDetailPage
   ));
 
   const { addToCart, cart, updateCartItemQuantity } = useCartStore();
-  const productInCart = cart.find((item) => item.product.id === productDetailData?.id);
-  const [quantity, setQuantity] = useState(productInCart ? productInCart.quantity : 0);
+  const productInCart = cart.find(
+    (item) => item.product.id === productDetailData?.id
+  );
+  const [quantity, setQuantity] = useState(
+    productInCart ? productInCart.quantity : 0
+  );
 
   const handleIncrement = () => {
     const updatedQuantity = quantity + 1;
@@ -53,8 +60,8 @@ export function ProductDetailPageBanner({ productDetailData }: ProductDetailPage
         description: productDetailData?.description,
         images: productDetailData?.images,
         category: productDetailData?.category,
-        creationAt: '',
-        updatedAt: '',
+        creationAt: "",
+        updatedAt: "",
       });
     }
   };
@@ -72,14 +79,23 @@ export function ProductDetailPageBanner({ productDetailData }: ProductDetailPage
 
   return (
     <Container my="md" fluid>
-      <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md" style={{ minHeight: 'fit-content' }}>
-        <Card radius="md" withBorder className={classes.imageContainer} p={'xl'}>
+      <SimpleGrid
+        cols={{ base: 1, sm: 2 }}
+        spacing="md"
+        style={{ minHeight: "fit-content" }}
+      >
+        <Card
+          radius="md"
+          withBorder
+          className={classes.imageContainer}
+          p={"xl"}
+        >
           <Carousel
             nextControlProps={{
-              style: { opacity: 1, backgroundColor: 'white' },
+              style: { opacity: 1, backgroundColor: "white" },
             }}
             previousControlProps={{
-              style: { opacity: 1, backgroundColor: 'white' },
+              style: { opacity: 1, backgroundColor: "white" },
             }}
             withIndicators
             loop
@@ -98,29 +114,45 @@ export function ProductDetailPageBanner({ productDetailData }: ProductDetailPage
               {productDetailData?.title}
             </Text>
           </Group>
-
+          <Group align="center">
+            <div>
+              <Rating
+                fractions={4}
+                readOnly
+                //between 2 and 5
+                defaultValue={
+                  (productDetailData.id % 3) + 2 + Math.random() * 0.5
+                }
+              />
+            </div>
+            <Text fz="sm" c="dimmed">
+              {productDetailData.id * productDetailData.price} reviews
+            </Text>
+          </Group>
           <Text fz="sm" c="dimmed" mt="sm">
             Category: {productDetailData?.category?.name}
           </Text>
-          <Text fz="sm" c="dimmed" mt="sm" mb={'xl'}>
+          <Text fz="sm" c="dimmed" mt="sm" mb={"xl"}>
             {productDetailData?.description}
           </Text>
 
           <Group justify="space-between" mt="auto">
             <div>
               <Text fz="xl" span fw={500} className={classes.price}>
-                {productDetailData?.price + '$'}
+                {productDetailData?.price + "$"}
               </Text>
             </div>
 
             {quantity > 0 ? (
-              <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+              <div
+                style={{ display: "flex", gap: "20px", alignItems: "center" }}
+              >
                 <ActionIcon
                   variant="filled"
                   aria-label="Decrement"
                   color="blue"
                   onClick={handleDecrement}
-                  size={'lg'}
+                  size={"lg"}
                 >
                   -
                 </ActionIcon>
@@ -130,7 +162,7 @@ export function ProductDetailPageBanner({ productDetailData }: ProductDetailPage
                   aria-label="Increment"
                   color="blue"
                   onClick={handleIncrement}
-                  size={'lg'}
+                  size={"lg"}
                 >
                   +
                 </ActionIcon>
