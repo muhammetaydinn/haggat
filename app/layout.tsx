7bae95824f3c24405d4b482decb7dbaedaa4045b
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-09-04 13:51:14 +0300

    fix: test module not found fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { MantineProvider } from "@mantine/core";
@@ -10,7 +9,6 @@ import Header from "@/components/Header";
 import Providers from "@/components/Providers";
 import { ReactQueryProvider } from "@/components/ReactQueryProvider";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HAGGAT - Gothic Things",
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full bg-white">
       <link rel="shortcut icon" href="/haggat_favicon.svg" />
-      <body className={`h-full ${inter.className} bg-whit text-black`}>
+      <body className={`h-full  bg-whit text-black`}>
         {/*Custom cache (optional) the code snippet below shows how to change the CSS key to css (the
         default is mui): */}
         <Providers>
